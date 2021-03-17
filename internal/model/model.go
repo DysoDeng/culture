35a79c343f52274094e2397da18baf26f0c2c8a4
栖枝 <dysodengs@gmail.com>
2021-03-17 13:01:39 +0800

    基础模型

--- internal/model/model.go
+++ internal/model/model.go
@@ -3,10 +3,15 @@ package model
 import "culture/cloud/base/internal/support/db"
 
 // 基础模型
-type ID struct {
-	ID        uint64  		`gorm:"primary_key;autoIncrement" json:"id"`
+//
+
+// 主键ID
+type PrimaryKeyID struct {
+	ID uint64 `gorm:"primary_key;autoIncrement" json:"id"`
 }
+
+// 添加时间,修改时间
 type Time struct {
-	CreatedAt db.JSONTime 	`gorm:"not null" json:"created_at"`
-	UpdatedAt db.JSONTime	`gorm:"not null" json:"updated_at"`
+	CreatedAt db.JSONTime `gorm:"type:datetime(0); not null" json:"created_at"`
+	UpdatedAt db.JSONTime `gorm:"type:datetime(0); not null" json:"updated_at"`
 }
